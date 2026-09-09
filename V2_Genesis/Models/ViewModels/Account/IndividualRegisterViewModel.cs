3f399b5de3d87466c65fc0b9e185708e5d27bec3
SibekoDev <conjugationjabulani@gmail.com>
2026-09-09 12:33:07 +0200

    Commit

--- V2_Genesis/Models/ViewModels/Account/IndividualRegisterViewModel.cs
+++ V2_Genesis/Models/ViewModels/Account/IndividualRegisterViewModel.cs
@@ -43,8 +43,6 @@ namespace V2_Genesis.Models.ViewModels.Account
         [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
         [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; } = string.Empty;
-
-        [Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the Terms of Use and POPIA Notice.")]
         [Display(Name = "Accept Terms")]
         public bool AcceptTerms { get; set; }
     }
